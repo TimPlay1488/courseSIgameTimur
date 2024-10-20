8d193ef747ac78aea5dce65d69735b869e6de5ad
Papls999 <Danrze12@gmail.com>
2024-10-20 15:55:16 +0200

    tedt

--- Game.py
+++ Game.py
@@ -1,6 +1,7 @@
 import sys
 import pygame as pg
 import time
+import random
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -10,6 +11,10 @@ SCREEN_HEIGHT = 800
 
 screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pg.time.Clock()
+TOP_BORDER = pg.Rect(0,0,SCREEN_WIDTH,1)
+BOTTOM_BORDER = pg.Rect(0,SCREEN_HEIGHT,SCREEN_WIDTH,1)
+LEFT_BORDER = pg.Rect(0,0,1, SCREEN_HEIGHT)
+RIGHT_BORDER = pg.Rect(SCREEN_WIDTH,0,1,SCREEN_HEIGHT)
 
 PLATFORM_WIDTH = 200
 PLATFORM_HEIGHT = 30
@@ -22,6 +27,7 @@ BALL_HEIGHT = 30
 BALL_X = (SCREEN_WIDTH - PLATFORM_WIDTH) // 2
 BALL_Y = 90
 BALL_SPEED = 3
+BALL_DIRECTION = pg.math.Vector2(1,1).normalize()
 
 FPS = 120
 
@@ -40,7 +46,9 @@ while True:
         PLATFORM_X += 3
         PLATFORM_X = min(SCREEN_WIDTH - PLATFORM_WIDTH, PLATFORM_X)
 
-    BALL_Y += BALL_SPEED
+    speed_vector = BALL_DIRECTION * BALL_SPEED
+    BALL_Y += speed_vector.y
+    BALL_X += speed_vector.x
     
     screen.fill(WHITE)
 
@@ -51,14 +59,28 @@ while True:
     pg.draw.rect(screen, BLACK, ball)
 
     if ball.colliderect(platform):
-        BALL_SPEED = -BALL_SPEED
+      t = random.Random().random()/2
+      print(t)
+      BALL_DIRECTION = BALL_DIRECTION.reflect(pg.math.Vector2(t,-1).normalize())
 
-    if BALL_Y < 0:
-        BALL_SPEED = -BALL_SPEED
+    if ball.colliderect(TOP_BORDER):
+      BALL_DIRECTION = BALL_DIRECTION.reflect(pg.math.Vector2(0,1))
 
-    if BALL_Y > 750:
-        BALL_SPEED = -BALL_SPEED
-        sys.exit()
+    if ball.colliderect(BOTTOM_BORDER):
+      BALL_DIRECTION = BALL_DIRECTION.reflect(pg.math.Vector2(0,-1))
+
+    if ball.colliderect(LEFT_BORDER):
+      BALL_DIRECTION = BALL_DIRECTION.reflect(pg.math.Vector2(1,0))
+
+    if ball.colliderect(RIGHT_BORDER):
+      BALL_DIRECTION = BALL_DIRECTION.reflect(pg.math.Vector2(-1,0))        
+
+    # if BALL_Y < 0:
+    #     BALL_SPEED = -BALL_SPEED
+
+    # if BALL_Y > 750:
+    #     BALL_SPEED = -BALL_SPEED
+    #     sys.exit()
 
     pg.display.flip()
 
